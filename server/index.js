a4ee978abc9035d66808522deadb78706055a7eb
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-11-25 17:16:25 +0100

    connexion DB mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
-import { Express } from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+/*import express from "express";
 import bodyParser from "body-parser";
-import { Mongoose } from "mongoose";
-import cors from "cors";
+import mongoose from "mongoose";
+import cors from "cors";*/
 
 const app = express();
 
@@ -10,12 +15,17 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = // créer une sécurité pour cacher le pseudo et mdp
-  "mongodb+srv://WaynePlanchenault:Wark-vadas1@freecluster.nz1xu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://WaynePlanchenault:WayneADMIN@freecluster.nz1xu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-Mongoose.connect(CONNECTION_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => app.listen);
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
 // https://www.mongodb.com/cloud/atlas   (heberge la DB sur leur cloud)
