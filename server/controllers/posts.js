bd5b7a52d4eacce8f85d795e54dbe39731998bd1
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-11-26 10:15:49 +0100

    controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,12 +1,29 @@
 const PostMessage = require("../models/postMessage.js");
 
-const getPosts = (req, res) => {
-  res.send("THIS WORKS");
+const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
-const createPost = (req, res) => {
-  res.send("Post creation");
+const createPost = async (req, res) => {
+  const body = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+    // error 409 La requête ne peut être traitée en l’état actuel.
+  }
 };
 
-module.exports = getPosts;
-module.exports = createPost;
+module.exports = { getPosts: getPosts, createPost: createPost };
+// si double ligne de module exports Error: Route.get() requires a callback function but got a [object Undefined]
